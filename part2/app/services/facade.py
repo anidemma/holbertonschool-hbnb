6c6553f5839d3cc5c188b580475bf12f1b5796b2
AliyevaMadina <mdn.alyv@gmail.com>
2024-10-27 20:06:51 +0400

    task 04

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,6 +1,7 @@
 from app.persistence.repository import InMemoryRepository
 from app.models.user import User
 from app.models.amenity import Amenity
+from app.models.place import Place
 
 
 class HBnBFacade:
