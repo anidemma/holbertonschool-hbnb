c1d8dcf0cfa09e2e0cf2e8daf435e47533ce1ac8
AliyevaMadina <mdn.alyv@gmail.com>
2024-10-27 19:53:10 +0400

    task 01

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,4 +1,6 @@
 from app.persistence.repository import InMemoryRepository
+from app.models.user import User
+
 
 class HBnBFacade:
     def __init__(self):
@@ -7,12 +9,35 @@ class HBnBFacade:
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
-    # Placeholder method for creating a user
     def create_user(self, user_data):
-        # Logic will be implemented in later tasks
-        pass
+        User.validate_request_data(user_data)
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
 
-    # Placeholder method for fetching a place by ID
-    def get_place(self, place_id):
-        # Logic will be implemented in later tasks
-        pass
+
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
+
+
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
+
+
+    def get_all_users(self):
+        return list(self.user_repo.get_all())
+
+
+    def update_user(self, user_id, user_data):
+        User.validate_request_data(user_data)
+        obj = self.get_user(user_id)
+        if obj:
+            obj.update(user_data)
+        return obj
+
+
+    def delete_user(self, user_id):
+        user = self.user_repo.get(user_id)
+        if user:
+            self.user_repo.delete(user_id)
+            return {"message": "User deleted successfully"}
