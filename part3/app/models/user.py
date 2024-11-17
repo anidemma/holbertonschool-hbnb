179e24514e3ae995c87702c71ec78ce0c5dce1b2
AliyevaMadina <mdn.alyv@gmail.com>
2024-11-17 23:04:23 +0400

    password added

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -2,25 +2,35 @@ import uuid
 from datetime import datetime
 from app.models.__init__ import BaseModel
 import re
+from flask_bcrypt import bcrypt
 
 
 regex = r'^[\w\.-]+@[\w\.-]+\.\w+$'
 
 
 class User(BaseModel):
-    def __init__(self, first_name, last_name, email):
+    def __init__(self, first_name, last_name, email, password):
         super().__init__()
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.is_admin = False
         self.places = []
+        self._password = password
 
     def save(self):
         super().save()
 
     def update(self, data):
         super().update(data)
+
+    def hash_password(self, password):
+        #Hashes the password before storing it.
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+    
+    def verify_password(self, password):
+        #Verifies if the provided password matches the hashed password.
+        return bcrypt.check_password_hash(self.password, password)
 
     @staticmethod
     def validate_request_data(data: dict):
