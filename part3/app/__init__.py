e3e5ff0fcf2064f1f2c9c7bab398b98ae5d6812e
AliyevaMadina <mdn.alyv@gmail.com>
2024-11-17 22:50:21 +0400

    updated app/__init__.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -2,10 +2,9 @@ from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
 
-def create_app():
+def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
+    app.config.from_object(config_class)
 
-    # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
     return app
